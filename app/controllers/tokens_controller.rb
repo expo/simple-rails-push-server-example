65ce2d4a23614876eb41f937cc25c6c26b24ec3f
Brent Vatne <brentvatne@gmail.com>
2017-07-28 16:41:02 -0700

    Only send push notification once

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -9,18 +9,16 @@ class TokensController < ApplicationController
     # otherwise it's not particularly useful
     @token = Token.where(value: params[:token][:value]).first
 
-    if @token.present?
-      message = 'Welcome back!'
-    else
+    if @token.blank?
       @token = Token.create(token_params)
       message = 'Hello there! This is a push notification.'
-    end
 
-    exponent.publish(
-      exponentPushToken: @token.value,
-      message: message,
-      data: {a: 'b'}, # Any arbitrary data to include with the notification
-    )
+      exponent.publish(
+        exponentPushToken: @token.value,
+        message: message,
+        data: {a: 'b'}, # Any arbitrary data to include with the notification
+      )
+    end
 
     render json: {success: true}
   end
